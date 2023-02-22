84893889fd2143033efb0235e523f00cc2574ee7
shypileu_m <shypileu_m@lwo.by>
2023-02-22 10:25:21 +0300

    feat: prisma integration and project config
    master

--- db/initial.sql
+++ db/initial.sql
@@ -1,1 +1,9 @@
 CREATE schema test;
+
+CREATE TABLE test."User" (
+	id serial4 NOT NULL,
+	email text NOT NULL,
+	name text NOT NULL,
+	CONSTRAINT "User_pkey" PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX "User_email_key" ON test."User" USING btree (email);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import express from "express";
 const routers = require("./routers");
 
@@ -8,5 +10,5 @@ const PORT = process.env.PORT || 3100;
 app.use("/api", routers);
 
 app.listen(PORT, () => {
-  console.log("some text");
+  console.log(`You listen PORT ${PORT}`);
 });
